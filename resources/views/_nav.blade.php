21694e6ba33273675fe1559b9353cc16efd72b1c
Swetha Sethumadhavan <swetha.sethumadhavan@gmail.com>
2018-01-16 10:33:49 -0700

    Top Navigation

---
+++ resources/views/_nav.blade.php
@@ -0,0 +1,61 @@
+ <nav class="navbar my-navbar navbar-fixed-top navbar-expand-md navbar-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#" id="textcolor">Our World</a>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0 navdisplay">
+      <li class="nav-item active ">
+        <a class="nav-link" href="#" id="textcolor">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="textcolor">Post</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="textcolor">Pics</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#" id="textcolor">Videos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="textcolor">Calendar</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link" id="textcolor">Bio</a>
+      </li>
+    </ul>
+    <a href="#" class="nav-item navbar-right">Login </a>
+    <!--
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    -->
+  </div>
+</nav>
+
+<!--
+<nav class="navbar my-navbar navbar-fixed-top">
+<div class="container">
+  <div class="navbar-left">
+     
+        <a href="#" class="navbar-brand">Our World</a>
+     
+
+      <a href="#" class="nav-item">Home</a> 
+      <a href="#" class="nav-item">Post</a>
+      <a href="#" class="nav-item">Pics</a>
+      <a href="#" class="nav-item">Videos</a>
+      <a href="#" class="nav-item">Calendar</a>
+      <a href="#" class="nav-item">Bio</a>
+   </div>
+<div class= "navbar-right">
+  <a href="#" class="nav-item">Login</a>
+
+</div>
+
+
+</div>
+</nav>
+-->
